09e7bc998ce016d01b33aa18a5ba631e7c7efec8
Sumit Saha <sumit.saha@appdynamics.com>
2019-07-04 19:46:54 +0530

    adding sample producer class code to send the data.

---
+++ src/main/java/KafkaProducerApp.java
@@ -0,0 +1,35 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class KafkaProducerApp {
+
+    private static final String REPLICATED_TOPIC = "my_replicated_topic";
+
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        // Need to provide at leaset 1 broker details as producer will fetch other broker details from the metadata that zookepeer would send
+        properties.put("bootstrap.servers", "localhost:9092, localhost:9093, localhost:9094");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> myProducer = new KafkaProducer<String, String>(properties);
+
+        ProducerRecord<String, String> producerRecord;
+
+        try {
+            for (int i = 0; i < 150; ++i ) {
+                producerRecord = new ProducerRecord<String, String>(REPLICATED_TOPIC, Integer.toString(i), "MyMessage: " + i);
+                myProducer.send(producerRecord);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            myProducer.close();
+        }
+
+
+    }
+}
